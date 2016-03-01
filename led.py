c0f3d59cf05bbb5be212293e14ce3223f4765867
TheGreatestAlan <alan.hoang.nguyen@gmail.com>
2016-02-29 21:38:41 -0700

    Working version for controlling servo

--- led.py
+++ led.py
@@ -1,3 +1,5 @@
+import time
+import curses
 import smbus
 import time
 
@@ -5,6 +7,21 @@ bus = smbus.SMBus(1)
 
 
 address = 0x04
+prevcommand = 0
+def main(stdscr):
+	stdscr.nodelay(1)
+	while True:
+		command = stdscr.getch()
+		if command != -1:
+			stdscr.addstr(str(command) + ' ')
+			if( command == 260):
+				command = 1
+			if( command == 261):
+				command = 2
+			writeNumber(command)
+			stdscr.refresh()
+			stdscr.move(0,0)
+			readNumber()
 
 def writeNumber(value):
 	bus.write_byte(address,value)
@@ -14,16 +31,5 @@ def readNumber():
 	number = bus.read_byte(address)
 	return number
 
-while True:
-	var = input("Enter 1-9:")
-	if not var:
-		continue
-
-writeNumber(var)
-print "RPI: Hi Arduino, I sent you ", var
-time.sleep(1)
-
-number = readNumber()
-print "Arduino: Hey RPI, I received a digit ", number
-print
-
+if __name__ == '__main__':
+	curses.wrapper(main)
